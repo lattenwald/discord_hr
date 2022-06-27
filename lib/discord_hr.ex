113453748d1820b03617bcd7600068e1061efe07
Alexander Q <qalexx@gmail.com>
2022-06-27 14:47:59 +0300

    logging

--- lib/discord_hr.ex
+++ lib/discord_hr.ex
@@ -1,16 +1,31 @@
 defmodule DiscordHr do
   alias Nostrum.Api
+  require Logger
 
   use Bitwise, only_operators: true
   # https://discord.com/developers/docs/resources/channel#message-object-message-flags
   @flag_ephemeral 1 <<< 6
 
   def respond_to_interaction(interaction, text, components \\ nil) do
-    Api.create_interaction_response(interaction, %{type: 4, data: %{content: text, flags: @flag_ephemeral, components: components}})
+    case Api.create_interaction_response(interaction, %{type: 4, data: %{content: text, flags: @flag_ephemeral, components: components}}) do
+      {:error, %{response: %{message: msg, errors: errors}}} ->
+        Logger.error "error responding to interaction: '#{msg}' #{inspect errors}"
+      {:error, err} ->
+        Logger.error "error responding to interaction: #{inspect err}"
+      {:ok} ->
+        :ok
+    end
   end
 
   def respond_to_component(interaction, text, components \\ []) do
-    Api.create_interaction_response(interaction, %{type: 7, data: %{content: text, components: components}})
+    case Api.create_interaction_response(interaction, %{type: 7, data: %{content: text, components: components}}) do
+      {:error, %{response: %{message: msg, errors: errors}}} ->
+        Logger.error "error responding to component: '#{msg}' #{inspect errors}"
+      {:error, err} ->
+        Logger.error "error responding to component: #{inspect err}"
+      {:ok} ->
+        :ok
+    end
   end
 
 end
